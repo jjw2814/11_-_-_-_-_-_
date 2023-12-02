5940ac0a090c8cd5d4f8a4807aa00817e5560235
박석환 <64780243+parkseokhwan0611@users.noreply.github.com>
2023-12-02 15:31:15 +0900

    Update bus_info

--- Design/HomeAssistant/bus_info.py
+++ Design/HomeAssistant/bus_info.py
@@ -1,1 +1,12 @@
+import requests
+import json
 
+url = 'https://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList?serviceKey=7fbmhI1JTcfdHRUVxPC1Q5dMRWaGT1iO9DR2RfJtxsghQ7Xc4tqtwAi3rBu5vIZmStXhsobOsYAC7m8qUCS3GA%3D%3D&pageNo=1&numOfRows=100&_type=json&cityCode=37010&nodeId=PHB351001140'
+response = requests.get(url)
+data = response.json()
+
+bus_info = {}
+for item in data['response']['body']['items']['item']:
+    bus_info[item['routeno']] = round(item['arrtime']/60)
+
+print(json.dumps(bus_info))
